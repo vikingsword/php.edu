8b29b70e55ce0086bf6600f4f780bfb973290780
vikingar <423141759@qq.com>
2024-04-29 11:22:50 +0800

    test

--- 0418/test/test4.php
+++ 0418/test/test4.php
@@ -2,13 +2,35 @@
 
 
 function getName($name) {
-    return $name;
+    return $name . '<br>';
 }
 
 $test = 'zs';
 $temp = getName($test);
 
 echo $temp;
+
+
+function fun1(float $f1) {
+    echo $f1 * 2.2 . '<br>';
+}
+
+fun1(1.1);
+
+function fun2(float $f1, $num = 2) {
+    echo $f1 * $num . '<br>';
+}
+
+fun2(2.2);
+
+$func1 = function fun3(float $f1, float $f2, float $f3) {
+    
+    echo $f1 + $f2 + $f3;
+};
+
+
+
+
 
 ?>
 
