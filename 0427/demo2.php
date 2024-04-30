41c63cb295cbf6f145544aa60359ebd234c6a0db
vikingar <423141759@qq.com>
2024-04-30 19:40:57 +0800

    edu

---
+++ 0427/demo2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>多文件上传1</title>
+</head>
+
+<body>
+<form action="" method="POST" enctype="multipart/form-data">
+    <fieldset>
+        <legend>多文件上传:逐个上传</legend>
+        <input type="file" name="my_pic1">
+        <input type="file" name="my_pic2">
+        <input type="file" name="my_pic3">
+        <button>上传</button>
+    </fieldset>
+</form>
+</body>
+
+</html>
