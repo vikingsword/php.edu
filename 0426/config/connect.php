2ce8aa258c3be6ab7ef4beba184bef5c32c7b7ca
vikingar <423141759@qq.com>
2024-04-30 13:25:24 +0800

    edu

--- 0426/config/connect.php
+++ 0426/config/connect.php
@@ -8,14 +8,14 @@ $dbConfig = require 'database.php';
 extract($dbConfig);
 
 // 1.dns
-$tp1 = '%s:host=%s;dbname=%s;port=%s;charset=%s';
+$tpl = '%s:host=%s;dbname=%s;port=%s;charset=%s';
 $args = [$type, $host, $dbname, $port, $charset];
-$dns = sprintf($tp1, ...$args);
-echo $dns;
+$dns = sprintf($tpl, ...$args);
+//echo $dns;
 
 // 2. 创建数据库对象
 $db = new PDO($dns, $username, $password);
-var_dump($db);
+//var_dump($db);
 
 // 设置结果集的默认获取部分
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, Pdo::FETCH_ASSOC);
--- 0426/demo2.php
+++ 0426/demo2.php
@@ -9,3 +9,44 @@ namespace php_edu;
  */
 
 // 1. 连接数据库
+require __DIR__ . '/config/connect.php';
+
+// 2. CURD: INSERT
+
+/**
+ * PDO预处理
+ * 为什么要用预处理?
+ * 1. 防止SQL注入攻击, 2. 数据延迟绑定
+ * (编程时只写SQL语句模板,执行SQL时再给占位符绑定真实数据)
+ * 预处理过程:
+ * 1. 创建SQL语句模板对象: 数据使用占位符表示
+ * 2. 执行SQL语句,根据操作类型(写/读),读返回结果集/数组, 写返回受影响的记录数量
+ */
+
+// INSERT 插入
+// INSERT 表名 SET 字段1=值1, 字段2=值2, ....
+// SQL语句的推荐规范:
+// 1. 关键字全大写
+// 2. 表名,字段名使用反引号做为定界符
+$sql = 'INSERT `staff` SET `name` = ?, `sex` = ?, `email` = ?';
+
+// 1. 创建SQL语句模板对象
+/** @var TYPE_NAME $db */
+$stmt = $db->prepare($sql);
+
+// 2. 执行SQL语句
+$stmt->execute(['小龙女', 1, 'xiaolongnv@php.cn']);
+$stmt->execute(['洪七公', 0, 'hongqigong@php.cn']);
+$stmt->execute(['黄蓉', 0, 'huangrong@php.cn']);
+$stmt->execute(['杨过', 0, 'yangguo@php.cn']);
+$stmt->execute(['猪老师', 0, 'zhulaoshi@php.cn']);
+$stmt->execute(['灭绝', 0, 'miejue@php.cn']);
+
+// 成功
+// $stmt->rowCount(): 返回受影响的记录数量
+if ($stmt->rowCount() > 0) {
+    echo '新增成功, 新增记录的主键ID = ' . $db->lastInsertId();
+} else {
+    echo '新增失败';
+    print_r($stmt->errorInfo());
+}
